8206e72ada513faae012a87641b164dc4d0f13b8
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2017-02-22 23:03:16 -0800

    adjusted comments

--- app.js
+++ app.js
@@ -23,13 +23,14 @@ app.get("/", (req, res) =>
 // Form submission
 
 // Stripe requires a very particular order of functions here.
-// 1. Create a new customer.
-// 2. You must evaluate a sku for a product. Unfortunately, Stripe does not support a variable price on a product. The only way to do this is to create multiple skus of one product, each with their own price. You can create a sku at the time of purchase but since the only attribute of the sku is price, you'll get an error back if you try to make a new sku for a price that's already been created by another customer.
-// 3. You must query the existing skus to see if ones already exists with the price your customer has entered. If there is not, you proceed with a new sku. If there is, you must use that existing sku.
-// 4. Create an order for said sku.
-// 5. Create a charge.
+// 1. You must evaluate a sku for a product. Unfortunately, Stripe does not support a variable price on a product. The only way to do this is to create multiple skus of one product, each with their own price. You can create a sku at the time of purchase but since the only attribute of the sku is price, you'll get an error back if you try to make a new sku for a price that's already been created by another customer. You must query the existing skus to see if ones already exists with the price your customer has entered. If there is not, you proceed with a new sku. If there is, you must use that existing sku.
+// 2. Create a new customer.
+// 3. Create an order for said sku.
+// 4. Create a charge.
+// Note: be sure your productId (defined in index.ejs) matches your "Gift Card" product in Stripe. Also make sure this product has the attribute "loadedamount".
+// if you get lost, Stripe's API docs are super concise: https://stripe.com/docs/api
 
-function evaluateSku(form) {
+function getSku(form) {
   console.log(form);
   var newSku;
   stripe.skus.list({
@@ -43,9 +44,8 @@ function evaluateSku(form) {
       existingSku = _.filter(skus.data, {
         'price': form.stripeAmount * 100
       });
-      newSku = existingSku[0].id;
-      console.log("This price already exists for the price: $" + form.stripeAmount + ", using sku: " + newSku);
-      completeOrder(form, newSku);
+      //console.log("This price already exists for the amout $" + form.stripeAmount + ", using sku: " + newSku);
+      completeOrder(form, existingSku[0].id);
     } else {
       stripe.skus.create({
         product: form.productId,
@@ -61,9 +61,8 @@ function evaluateSku(form) {
         if (err) {
           console.log(err)
         }
-        newSku = sku.id;
-        console.log("$" + form.stripeAmount + " is a new price. We made a new sku: " + sku.id);
-        completeOrder(form, newSku);
+        //console.log("$" + form.stripeAmount + " is a new price. We made a new sku: " + sku.id);
+        completeOrder(form, sku.id);
       })
     }
   })
@@ -109,7 +108,7 @@ function completeOrder(form, sku) {
 }
 
 app.post('/thanks', function (req, res) {
-  evaluateSku(req.body, res);
+  getSku(req.body);
   res.render("thanks.ejs")
 })
 
