b7c5270915d4d81484627629e2ead740a919866b
Jeremy P. Beasley <jbeasley@starbucks.com>
2017-03-28 00:00:29 -0700

    cleaning up comments and docs

--- app.js
+++ app.js
@@ -23,27 +23,32 @@ app.get("/", (req, res) =>
 // Form submission
 
 function getSku(form) {
-  console.log(form);
   var newSku;
+  // get all existing skus and list them out
   stripe.skus.list({
     limit: 30
   }, function(err, skus) {
     if (err) {
       console.log(err)
     }
+    // make a list of price-points that already exist for the gift card
     var sortedskus = _.map(skus.data, "price")
+    // see if the price we're trying to submit already exists
     if (sortedskus.includes(form.stripeAmount * 100)) {
       existingSku = _.filter(skus.data, {
+        // since the form is submitting an integer, we must make it make "cents"
         'price': form.stripeAmount * 100
       });
-      //console.log("This price already exists for the amout $" + form.stripeAmount + ", using sku: " + newSku);
+      // use the existing sku
       completeOrder(form, existingSku[0].id);
     } else {
+      // or if it doesn't, just make a new one
       stripe.skus.create({
         product: form.productId,
         attributes: {
           'loadedamount': form.stripeAmount
         },
+        // since the form is submitting an integer, we must make it make "cents"
         price: form.stripeAmount * 100,
         currency: 'usd',
         inventory: {
@@ -53,11 +58,99 @@ function getSku(form) {
         if (err) {
           console.log(err)
         }
-        //console.log("$" + form.stripeAmount + " is a new price. We made a new sku: " + sku.id);
+        //now you've got a sku, proceed to contruct the customer, order, and charge
         completeOrder(form, sku.id);
       })
     }
   })
+}
+
+function completeOrder(form, sku) {
+  // so now we have a sku, let's define some global variables that we're going to update along the way
+  var customerId;
+  var orderTotal = form.stripeAmount * 100;
+  // use the .customers method to make a new customer using the email and token, we also throw a phone number in there due to Canlis' needs
+  stripe.customers.create({
+    email: form.stripeEmail,
+    source: form.stripeToken,
+    metadata: {
+      customer_phone: form.customer_phone
+    }
+  }, function(err, customer) {
+    if (err) {
+      console.log(err)
+    }
+    // ok, customer created, let's save that id since we'll use it later
+    customerId = customer.id;
+    // now, construct the order
+    stripe.orders.create({
+      items: [{
+        parent: sku
+      }],
+      currency: "usd",
+      email: form.stripeEmail,
+      shipping: {
+        name: form.recipient_name,
+        address: {
+          line1: form.shipping_address_line1,
+          city: form.shipping_address_city,
+          state: form.shipping_address_state,
+          postal_code: form.shipping_address_postal_code
+        }
+      },
+      // we throw in some information as meta data so it can easily be seen from the Stripe dashboard at https://dashboard.stripe.com/orders without clicking on the customer
+      metadata: {
+        giftcard_amount: '$' + form.stripeAmount,
+        customer_name: form.customer_name,
+        customer_email: form.stripeEmail,
+        customer_phone: form.customer_phone,
+        recipient_name: form.recipient_name,
+        recipient_message: form.recipient_message
+      }
+    }, function(err, order) {
+      if (err) {
+        console.log(err)
+      }
+      // now, determine if we need to charge them for shipping based on what they selected in the form
+      var isFreeShipping;
+      if (form.shipping_preference == "pickup") {
+        isFreeShipping = true;
+      } else {
+        isFreeShipping = false;
+      }
+      // get the ids for free and standard shipping from the order object
+      if (isFreeShipping) {
+        function getFreeMethodId(method) {
+          return method.amount === 0;
+        }
+        var shippingId = order.shipping_methods.find(getFreeMethodId).id;
+        orderTotal += order.shipping_methods.find(getFreeMethodId).amount;
+      } else {
+        function getNotFreeMethodId(method) {
+          return method.amount === 400;
+        }
+        var shippingId = order.shipping_methods.find(getNotFreeMethodId).id;
+        orderTotal += order.shipping_methods.find(getNotFreeMethodId).amount;
+      }
+      // update the order object with the prefered shipping method
+      stripe.orders.update(order.id, {
+        selected_shipping_method: shippingId
+      });
+      // now, we construct a charge
+      stripe.charges.create({
+        amount: orderTotal,
+        currency: "usd",
+        customer: customerId
+      }, function(err, charge) {
+        if (err) {
+          console.log(err)
+        }
+        // ok, now everything's done. Form Success! Render thank you page.
+        // TEST LOG
+        // console.log("TOTAL CHARGE: $" + charge.amount / 100);
+      });
+    });
+  });
 }
 
 function updateShipping(orderId, methods, isFree) {
@@ -79,90 +172,9 @@ function updateShipping(orderId, methods, isFree) {
   })
 }
 
-function completeOrder(form, sku) {
-  //console.log("!!!!!! 1. SKU CONFIRMED !!!!!!");
-  var customerId;
-  var orderTotal = form.stripeAmount * 100;
-  stripe.customers.create({
-    email: form.stripeEmail,
-    source: form.stripeToken,
-    metadata: {
-      customer_phone: form.customer_phone
-    }
-  }, function(err, customer) {
-    if (err) {
-      console.log(err)
-    }
-    //console.log("!!!!!! 2. CUSTOMER CREATED !!!!!!");
-    customerId = customer.id;
-    stripe.orders.create({
-      items: [{
-        parent: sku
-      }],
-      currency: "usd",
-      email: form.stripeEmail,
-      shipping: {
-        name: form.recipient_name,
-        address: {
-          line1: form.shipping_address_line1,
-          city: form.shipping_address_city,
-          state: form.shipping_address_state,
-          postal_code: form.shipping_address_postal_code
-        }
-      },
-      metadata: {
-        giftcard_amount: '$' + form.stripeAmount,
-        customer_name: form.customer_name,
-        customer_email: form.stripeEmail,
-        customer_phone: form.customer_phone,
-        recipient_name: form.recipient_name,
-        recipient_message: form.recipient_message
-      }
-    }, function(err, order) {
-      if (err) {
-        console.log(err)
-      }
-      // Determining if we need to charge for shipping
-      var isFreeShipping;
-      if (form.shipping_preference == "pickup") {
-        isFreeShipping = true;
-      } else {
-        isFreeShipping = false;
-      }
-      // Updating shipping
-      if (isFreeShipping) {
-        console.log("SHIPPING: FREE");
-        function getFreeMethodId(method) {
-          return method.amount === 0;
-        }
-        var shippingId = order.shipping_methods.find(getFreeMethodId).id;
-        orderTotal += order.shipping_methods.find(getFreeMethodId).amount;
-      } else {
-        console.log("SHIPPING: NOT FREE");
-        function getNotFreeMethodId(method) {
-          return method.amount === 400;
-        }
-        var shippingId = order.shipping_methods.find(getNotFreeMethodId).id;
-        orderTotal += order.shipping_methods.find(getNotFreeMethodId).amount;
-      }
-      stripe.orders.update(order.id, {
-        selected_shipping_method: shippingId
-      });
-      stripe.charges.create({
-        amount: orderTotal,
-        currency: "usd",
-        customer: customerId
-      }, function(err, charge) {
-        if (err) {
-          console.log(err)
-        }
-      });
-    });
-  });
-}
-
 app.post('/thanks', function (req, res) {
   getSku(req.body);
+  // rendering the thanks screen should not happen here, it should happen on 150 when the entire form is complete.
   res.render("thanks.ejs")
 })
 
