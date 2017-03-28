775089756fb940f388d3b242757ba651d3d16db3
Jeremy P. Beasley <jbeasley@starbucks.com>
2017-03-27 22:28:58 -0700

    shipping message visible and working

--- public/master.js
+++ public/master.js
@@ -2,35 +2,41 @@ $( document ).ready(function() {
 
   // TESTING ONLY: FORM AUTO FILL
 
-  $('[name="customer_name"]').val("Kurt Cobain");
-  $('[name="customer_phone"]').val("2064567890");
-  $('[name="recipient_name"]').val("Courtney Love");
-  $('[name="recipient_message"]').val("Why'd u kill me?");
-  $('#ShipToPickup').prop("checked", true);
-  $('[name="shipping_address_line1"]').val("171 Lake Washington Blvd E");
-  $('[name="shipping_address_line2"]').val("");
-  $('[name="shipping_address_city"]').val("Seattle");
-  $('[name="shipping_address_state"]').val("Washington");
-  $('[name="shipping_address_country"]').val("United States");
-  $('[name="shipping_address_postal_code"]').val("98112");
-  $('[name="stripeEmail"]').val("kurtyboy@hotmail.com");
-  $('[name="stripeAmount"]').val("123");
-  $('#cc').val("4242424242424242");
-  $('#ccm').val("12");
-  $('#ccy').val("19");
-  $('#cvv').val("123");
+  // $('[name="customer_name"]').val("Kurt Cobain");
+  // $('[name="customer_phone"]').val("2064567890");
+  // $('[name="recipient_name"]').val("Courtney Love");
+  // $('[name="recipient_message"]').val("Why'd u kill me?");
+  // $('#ShipToPickup').prop("checked", true);
+  // $('[name="shipping_address_line1"]').val("171 Lake Washington Blvd E");
+  // $('[name="shipping_address_line2"]').val("");
+  // $('[name="shipping_address_city"]').val("Seattle");
+  // $('[name="shipping_address_state"]').val("Washington");
+  // $('[name="shipping_address_country"]').val("United States");
+  // $('[name="shipping_address_postal_code"]').val("98112");
+  // $('[name="stripeEmail"]').val("kurtyboy@hotmail.com");
+  // $('[name="stripeAmount"]').val("123");
+  // $('#cc').val("4242424242424242");
+  // $('#ccm').val("12");
+  // $('#ccy').val("19");
+  // $('#cvv').val("123");
 
   // Populates "You will be charged.." dialogue before submit button
 
+  var shippingCost = 0;
+  var enteredAmount;
+
+  function showTotalCharge() {
+    totalCharge = enteredAmount + shippingCost;
+    $("#YouWillBeCharged").show().text("You will be charged a total of $" + totalCharge + ".00");
+  }
+
   $("#YouWillBeCharged").hide()
   $(".GiftAmount input").keyup(function() {
-      var value = Number($(this).val());
-      if (value) {
-          var shippingCost = 4;
-          var totalCharge = value + shippingCost;
-          $("#YouWillBeCharged").show().text("You will be charged a total of $" + totalCharge + ".00");
+      enteredAmount = Number($(this).val());
+      if (enteredAmount) {
+        showTotalCharge();
       } else {
-          $("#YouWillBeCharged").hide().text("");
+        $("#YouWillBeCharged").hide().text("");
       };
   }).keyup();
 
@@ -41,12 +47,18 @@ $( document ).ready(function() {
   $('.ShippingSelector').click(function() {
     if($('#ShipToMe').is(':checked')) {
       $(".ShippingInformation").show();
+      shippingCost = 4;
+      showTotalCharge();
     }
     if ($('#ShipToRecipient').is(':checked')) {
       $(".ShippingInformation").show();
+      shippingCost = 4;
+      showTotalCharge();
     }
     if ($('#ShipToPickup').is(':checked')) {
       $(".ShippingInformation").hide();
+      shippingCost = 0;
+      showTotalCharge();
     }
   });
 
@@ -81,7 +93,7 @@ $( document ).ready(function() {
   });
 });
 
-// FORM VALIDATION
+// FORM VALIDATION & ERROR MESSAGES
 
 Stripe.setPublishableKey('pk_test_Gbu2akKhNgGjbKi4LPxOOWqc');
 
@@ -170,13 +182,13 @@ $("#payment-form").submit(function(event) {
     }
   });
   if(!$("#payment-form").valid()){
-    console.log("aint valid");
+    // console.log("aint valid");
     $('body, html').animate({ scrollTop: 0 }, 200);
     event.preventDefault();
     return false;
   }
   if($("#payment-form").valid()){
-    console.log("is valid");
+    // console.log("is valid");
     // Disable the submit button to prevent repeated clicks:
     $(this).find('.submit').prop('disabled', true);
     // Request a token from Stripe:
