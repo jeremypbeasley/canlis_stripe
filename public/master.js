7aaf870d5516eabea3e7037e55f0e2c0143f3198
Jeremy P. Beasley <jbeasley@starbucks.com>
2017-03-30 11:16:48 -0700

    working in live mode

--- public/master.js
+++ public/master.js
@@ -110,7 +110,7 @@ $('#ShipToPickup').click(function() {
 
 // FORM VALIDATION & ERROR MESSAGES
 
-Stripe.setPublishableKey('pk_test_qHOVbF8lzJGlNt53WAbNIGim');
+Stripe.setPublishableKey('pk_live_4kvjDESffDHa3yDxThoVTXUK');
 
 $("#payment-form").submit(function(event) {
   $("#payment-form").validate({
